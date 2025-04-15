f05a1449bf26e9a042186e13c315d60a7a1cbaee
hyeran0513 <hr_0513@naver.com>
2025-04-15 15:56:17 +0900

    250415 김혜란 커스텀버튼 생성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
+import CustomButton from "@/components/CustomButton";
 import { Text, SafeAreaView } from "react-native";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
       <Text>홈 스크린</Text>
+      <CustomButton label="버튼" onPress={() => {}} />
     </SafeAreaView>
   );
 };
