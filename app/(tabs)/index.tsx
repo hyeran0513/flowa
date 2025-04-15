99c50d31dc630ff1edc009553ff9adb9fda2229b
hyeran0513 <hr_0513@naver.com>
2025-04-15 16:40:52 +0900

    250415 김혜란 로그인 페이지 생성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 import { Text, SafeAreaView } from "react-native";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
       <Text>홈 스크린</Text>
-      <CustomButton label="버튼" onPress={() => {}} />
+      <CustomButton label="버튼" onPress={() => router.push("/auth")} />
     </SafeAreaView>
   );
 };
---
+++ app/auth/_layout.tsx
@@ -0,0 +1,31 @@
+import { Link, Stack } from "expo-router";
+import Foundation from "@expo/vector-icons/Foundation";
+import { colors } from "@/constants";
+
+const AuthLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerTintColor: colors.BLACK,
+        contentStyle: {
+          backgroundColor: colors.WHITE,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "로그인",
+          headerShown: true,
+          headerLeft: () => (
+            <Link href={"/"} replace style={{ paddingRight: 5 }}>
+              <Foundation name="home" size={28} color={colors.BLACK} />
+            </Link>
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default AuthLayout;
